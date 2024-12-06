33bd20f2d0e5c60682601495254809dda8ab5ffc
Ian Lockwood <mrianlockwood@gmail.com>
2024-12-06 11:12:36 -0500

    Add basic state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import words from './wordList.json';
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   return (
-    <h1>Hi</h1>
+    <h1>{wordToGuess}</h1>
   )
 }
 
