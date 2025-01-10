970f4c2c07f87be2a56963a445fa0e17f5c80b60
Ian Lockwood <mrianlockwood@gmail.com>
2025-01-10 12:01:56 -0500

    Add basic components

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,23 @@ function App() {
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   return (
-    <h1>{wordToGuess}</h1>
+    <div style={{
+      maxWidth: '800px',
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '2rem',
+      margin: '0 auto',
+      alignItems: 'center'
+    }}>
+      <div style={{fontSize: '2rem', textAlign: 'center'}}>
+        Lose
+        Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+
+    </div>
   )
 }
 
